86de9b2cda1354870cd7ac02864b6d56f45c4e00
Chaitanya Shetty <chaitanyashetty47@gmail.com>
2024-10-23 17:00:56 +0530

    changes

--- back/dist/config/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabase = void 0;
-const supabase_js_1 = require("@supabase/supabase-js");
-require("dotenv/config");
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
-const supabaseKey = process.env.SUPABASE_API_KEY;
-exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
